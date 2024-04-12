2bdf91d7f267f3b831768dd05eda40fd877e5eb1
photon341 <132476918+photon341@users.noreply.github.com>
2024-04-12 11:13:47 +0530

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -5,6 +5,60 @@
 
 using namespace std;
 
+int INT_check(string a);
+void gusse_number(int N, int secretNumber);
+int check_level(int l);
+
+
+int main(){
+	cout<<"Welcome to Guess The Number game!\n";
+	cout<<"______________________________________________________________________________________________________________\n";
+	cout<<"You have to guess a number between 1 and 100.\n";
+    cout<<"You'll have 10 chances to guess the correct number. Be careful. Good Luck!\n";
+    
+    while(true){
+	    cout << "\nEnter the choice: \n";
+	    cout << "1 for Play Game!\n";
+	    cout << "2 for Exit Game!\n\n";
+	    
+		// int* n = new int;
+		string Num;
+		int userChoice;
+		// *n = userChoice;    
+	    cout<<"Enter the number: ";
+	    cin>>Num;
+	    userChoice = INT_check(Num);
+	    srand(time(0));
+	    int SecretNumber = 1 + (rand() % 100);
+	    if(userChoice == 1){
+			string str_level;
+			int level, c=1;
+			while (c == 1)
+			{
+			cout<<"1 for Easy!\n";
+			cout<<"2 for Hard!\n";
+			cout<<"3 for Difficult!\n";
+			cout<<"Enter the Level: \n";
+			cin>>str_level;
+			level = INT_check(str_level);
+			if(check_level(level) == 1){
+				int N_choice = 10/level;
+				cout<<"\nYou have " <<(N_choice)<<" chance for finding the secret number between 1 and 100.";
+				gusse_number(N_choice, SecretNumber);
+				c=0;
+			}else{
+				cout<<"Wrong choice, Enter valid Level to play the game! (1, 2, 3)\n\n";
+			}
+		}
+	    } else if (userChoice == 2) {
+	        exit(0);
+	    } else {
+			cout << "Wrong choice, Enter valid choice to play the game! (1, 2)\n";
+		}
+	}
+	return 0;
+}
+
 int INT_check(string a){
 	int n;
 	try
@@ -67,55 +121,4 @@ int check_level(int l){
 	else{
 		return 0;
 	}
-}
-
-
-
-int main(){
-	cout<<"Welcome to Guess The Number game!\n";
-	cout<<"______________________________________________________________________________________________________________\n";
-	cout<<"You have to guess a number between 1 and 100.\n";
-    cout<<"You'll have 10 chances to guess the correct number. Be careful. Good Luck!\n";
-    
-    while(true){
-	    cout << "\nEnter the choice: \n";
-	    cout << "1 for Play Game!\n";
-	    cout << "2 for Exit Game!\n\n";
-	    
-		// int* n = new int;
-		string Num;
-		int userChoice;
-		// *n = userChoice;    
-	    cout<<"Enter the number: ";
-	    cin>>Num;
-	    userChoice = INT_check(Num);
-	    srand(time(0));
-	    int SecretNumber = 1 + (rand() % 100);
-	    if(userChoice == 1){
-			string str_level;
-			int level, c=1;
-			while (c == 1)
-			{
-			cout<<"1 for Easy!\n";
-			cout<<"2 for Hard!\n";
-			cout<<"3 for Difficult!\n";
-			cout<<"Enter the Level: \n";
-			cin>>str_level;
-			level = INT_check(str_level);
-			if(check_level(level) == 1){
-				int N_choice = 10/level;
-				cout<<"\nYou have " <<(N_choice)<<" chance for finding the secret number between 1 and 100.";
-				gusse_number(N_choice, SecretNumber);
-				c=0;
-			}else{
-				cout<<"Wrong choice, Enter valid Level to play the game! (1, 2, 3)\n\n";
-			}
-		}
-	    } else if (userChoice == 2) {
-	        exit(0);
-	    } else {
-			cout << "Wrong choice, Enter valid choice to play the game! (1, 2)\n";
-		}
-	}
-	return 0;
 }
